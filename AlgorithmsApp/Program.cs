68fb3ae57f9ae0c8f9779fe59be7aee3876bbfd1
Elio Cortes <elio.cortes3000@gmail.com>
2023-02-22 21:37:18 -0500

    Example Stack

--- AlgorithmsApp/Program.cs
+++ AlgorithmsApp/Program.cs
@@ -1,6 +1,9 @@
-﻿using AlgorithmsApp;
+﻿//using AlgorithmsApp;
 
-int[] array = new int[] { 3, 56, 73, 6, 75, 7, 3, 566, 88, 3, 64, 64, 64, 78, 3, 22, 45, 67, 7 };
-Bubble.Sort(ref array);
-Console.WriteLine(string.Join(" ", array));
+//int[] array = new int[] { 3, 56, 73, 6, 75, 7, 3, 566, 88, 3, 64, 64, 64, 78, 3, 22, 45, 67, 7 };
+//Bubble.Sort(ref array);
+//Console.WriteLine(string.Join(" ", array));
+using AlgorithmsApp;
+
+StackEx se=new StackEx();
 
