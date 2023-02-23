878bc8715039169d74fdc7cd7167f74006112c40
Elio Cortes <elio.cortes3000@gmail.com>
2023-02-22 22:11:58 -0500

    Example LinkedList

---
+++ AlgorithmsApp/LinkedListEx.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+
+namespace AlgorithmsApp
+{
+    internal class LinkedListEx
+    {
+        public LinkedListEx()
+        {
+            string[] words = new string[] { "First", "Second", "Third", "Fourth" };
+            LinkedList<string> ll = new LinkedList<string>(words);
+            ll.AddLast("Fifth");
+            ll.RemoveFirst();
+            ll.Remove("Third");
+            LinkedListNode<string> recent= ll.AddLast("Seventh");
+            ll.AddBefore(recent, "Sixth");
+
+            foreach (string word in ll)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
