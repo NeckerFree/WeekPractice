cf31100e10fb3412d008013e89996414b87a88c8
Elio Cortes <elio.cortes3000@gmail.com>
2023-02-22 22:26:28 -0500

    example Hashtable

---
+++ AlgorithmsApp/HashtableEx.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace AlgorithmsApp
+{
+    internal class HashtableEx
+    {
+        public HashtableEx()
+        {
+            Hashtable hashtable= new Hashtable();
+            hashtable.Add(3, "Three");
+            hashtable.Add(0, "Zero");
+            hashtable.Add(1, "One");
+            hashtable.Add(2, "Two");
+            foreach (var item in hashtable)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
