8089e11cf030a89a9c8091e81766d52b26c1a616
Elio Cortes <elio.cortes3000@gmail.com>
2023-02-22 20:58:24 -0500

    Implemented Bubbled Sort

---
+++ AlgorithmsApp/Program.cs
@@ -0,0 +1,6 @@
+﻿using AlgorithmsApp;
+
+int[] array = new int[] { 3, 56, 73, 6, 75, 7, 3, 566, 88, 3, 64, 64, 64, 78, 3, 22, 45, 67, 7 };
+Bubble.Sort(ref array);
+Console.WriteLine(string.Join(" ", array));
+
