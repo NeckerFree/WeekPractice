3d41e2ed8bcccfd16a41ab09a3881c5a06c7a8b9
Elio Cortes <elio.cortes3000@gmail.com>
2023-02-22 22:42:14 -0500

    Example Binary Search

---
+++ AlgorithmsApp/BinarySearchEx.cs
@@ -0,0 +1,17 @@
+﻿namespace AlgorithmsApp
+{
+    internal class BinarySearchEx
+    {
+        public BinarySearchEx() 
+            {
+                
+            int[] data = new int[12] { 1, 3, 5, 7, 11,1,76, 13, 17, 19, 23, 31 };
+            Array.Sort(data);
+            Console.WriteLine(string.Join(" ", data));
+            // Value to search for    
+            int target = 14;
+            int pos = Array.BinarySearch(data, target);
+            Console.WriteLine(pos);
+        }
+    }
+}
