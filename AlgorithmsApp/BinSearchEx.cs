a9e36d51b3fd23e94195ac6a80a137c11936d9dd
Elio Cortes <elio.cortes3000@gmail.com>
2023-03-02 15:30:15 -0500

    Recursive Binary Search

---
+++ AlgorithmsApp/BinSearchEx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsApp
+{
+    public class BinSearchEx
+    {
+        public int BinarySearchAlg(int[] arr, int start, int end, int key)
+        {
+            int position = -1;
+            if (start == end) { return position; }
+            int middle = start + (end - start) / 2;
+            if (arr[middle] > key)
+            {
+                position = BinarySearchAlg(arr, start, middle, key);
+                return position;
+            }
+            if (arr[middle] < key)
+            {
+                position = BinarySearchAlg(arr, middle + 1, end, key);
+                return position;
+            }
+            position = middle;
+            return position;
+        }
+    }
+}
