bf7bb719ca0510c08a683ced6ab3fe498bc5b286
Elio Cortes <elio.cortes3000@gmail.com>
2023-03-12 23:46:13 -0500

    Created method to match dominoes and unit tests

---
+++ AlgorithmsApp/Tuples.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsApp
+{
+    public class Tuples
+    {
+        public static bool CanChain(IEnumerable<(int, int)> dominoes)
+        {
+            bool canChain = false;
+            if (dominoes.Any() == false) return canChain;
+            var dominoesResult = new List<(int, int)>();
+            dominoes = dominoes.OrderBy(item =>(item.Item1, item.Item2));
+            foreach (var domino in dominoes)
+            {
+                bool dominoInlist = dominoesResult.Contains(domino);
+                int index = -1;
+                var dominoToProcess = (dominoInlist == false) ? domino: (domino.Item2, domino.Item1) ;
+                index = GetIndex(dominoesResult, dominoToProcess);
+                if (index == dominoesResult.Count)
+                {
+                    if (dominoToProcess == domino)
+                    {
+                        var dominoReverse = (domino.Item2, domino.Item1);
+                        var newindex = GetIndex(dominoesResult, dominoReverse);
+                        index = (newindex == dominoesResult.Count) ? index : newindex;
+                        dominoToProcess = (newindex == dominoesResult.Count) ? domino : dominoReverse;
+                    }
+                }
+                if (index == 0 && dominoesResult.Count>0)
+                {
+                    if (dominoesResult[index].Item1 == dominoToProcess.Item1)
+                    {
+                        var dominoReverse = (domino.Item2, domino.Item1);
+                        dominoToProcess = dominoReverse;
+                    }
+                }
+                dominoesResult.Insert(index, dominoToProcess);
+
+            }
+
+            var first = dominoesResult.First();
+            var last = dominoesResult.Last();
+            canChain = (first.Item1 == last.Item2);
+            return canChain;
+        }
+
+        private static int GetIndex(List<(int, int)> dominoesResult, (int, int) domino)
+        {
+            var nextDomino = dominoesResult.Where(elem => elem.Item2 == domino.Item1).FirstOrDefault();
+            int index = dominoesResult.IndexOf(nextDomino);
+            if (index != -1)
+            {
+                if (index != 0)
+                {
+                    var previous = dominoesResult[index - 1];
+                    if (previous.Item2 == nextDomino.Item1)
+                    {
+                        index = dominoesResult.Count;
+                    }
+                }
+                //else
+                //{
+                //    index = 1;
+                //}
+            }
+            else
+            {
+                (int, int) reverse = (domino.Item2, domino.Item1);
+                var foundReverse = dominoesResult.Where(elem => elem.Item2 == reverse.Item1).FirstOrDefault();
+                int indexReverse = dominoesResult.IndexOf(foundReverse);
+                if (indexReverse != -1)
+                {
+                    var previous = dominoesResult[indexReverse - 1];
+                    if (previous.Item2 == foundReverse.Item1)
+                    {
+                        index = dominoesResult.Count;
+                    }
+                }
+                else
+                {
+                    index = dominoesResult.Count;
+                }
+
+            }
+
+            return index;
+        }
+    }
+}
---
+++ TestProject/DominoesTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    using AlgorithmsApp;
+    using System;
+    using Xunit;
+    public class DominoesTests
+    {
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Empty_input_empty_output()
+        {
+            var dominoes = Array.Empty<(int, int)>();
+            Assert.True(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Singleton_input_singleton_output()
+        {
+            var dominoes = new[] { (1, 1) };
+            Assert.True(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Singleton_that_cant_be_chained()
+        {
+            var dominoes = new[] { (1, 2) };
+            Assert.False(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Three_elements()
+        {
+            var dominoes = new[] { (1, 2), (3, 1), (2, 3) };
+            Assert.True(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Can_reverse_dominoes()
+        {
+            var dominoes = new[] { (1, 2), (1, 3), (2, 3) };
+            Assert.True(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Cant_be_chained()
+        {
+            var dominoes = new[] { (1, 2), (4, 1), (2, 3) };
+            Assert.False(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Disconnected_simple()
+        {
+            var dominoes = new[] { (1, 1), (2, 2) };
+            Assert.False(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Disconnected_double_loop()
+        {
+            var dominoes = new[] { (1, 2), (2, 1), (3, 4), (4, 3) };
+            Assert.False(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Disconnected_single_isolated()
+        {
+            var dominoes = new[] { (1, 2), (2, 3), (3, 1), (4, 4) };
+            Assert.False(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Need_backtrack()
+        {
+            var dominoes = new[] { (1, 2), (2, 3), (3, 1), (2, 4), (2, 4) };
+            Assert.True(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Separate_loops()
+        {
+            var dominoes = new[] { (1, 2), (2, 3), (3, 1), (1, 1), (2, 2), (3, 3) };
+            Assert.True(Tuples.CanChain(dominoes));
+        }
+        [Fact]
+        public void Nine_elements()
+        {
+            var dominoes = new[] { (1, 2), (5, 3), (3, 1), (1, 2), (2, 4), (1, 6), (2, 3), (3, 4), (5, 6) };
+            Assert.True(Tuples.CanChain(dominoes));
+        }
+        [Fact(Skip = "Remove this Skip property to run this test")]
+        public void Separate_three_domino_loops()
+        {
+            var dominoes = new[] { (1, 2), (2, 3), (3, 1), (4, 5), (5, 6), (6, 4) };
+            Assert.False(Tuples.CanChain(dominoes));
+        }
+    }
+}
+
+
