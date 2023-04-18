bcfcf4408c5ff8e1beda22836ee71abed1c3e571
Elio Cortés <elio.cortes3000@gmail.com>
2023-04-17 21:50:50 -0500

    Implemented fundamentals exercises

---
+++ AlgorithmsApp/Fundamentals/B.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmsApp.Fundamentals
+{
+    public class B:A
+    {
+        public new void m1(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AlgorithmsApp/Fundamentals/Heredada.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsApp.Fundamentals
+{
+    public class Heredada: Abstracta
+    {
+        public override void Method1()
+        {
+            base.Method1();
+        }
+        public Heredada()
+        {
+            int val = 49;
+            ModifyVal(ref val);
+        }
+
+        private void ModifyVal(ref int val)
+        {
+            val = 99;
+        }
+    }
+
+    public interface IMyStruct
+    {
+        void Process();
+        public void Save();
+    }
+    public struct MyStruct2 : IMyStruct
+    {
+        public void Process()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public
+        enum Enum : int
+        {
+        First,
+        Second,
+        Third,
+        }
+}
