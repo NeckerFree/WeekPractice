cf91222f2f3deb4e2f3c64334a115ca96dd9e3ff
Elio Cortés <elio.cortes3000@gmail.com>
2023-05-03 20:26:10 -0500

    Solved Challenge https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem

--- TestProject/StringOperationsTests.cs
+++ TestProject/StringOperationsTests.cs
@@ -1,4 +1,6 @@
 ﻿using AlgorithmsApp.Classes;
+using AlgorithmsApp.HackerRank;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,63 @@ namespace TestProject
     public class StringOperationsTests
     {
         [Fact]
-        public void ValidatePalindrome() {
-            StringOperations  stringOperations = new StringOperations();
-         bool actual=   stringOperations.IsPalindrome("aabaa");
+        public void ValidatePalindrome()
+        {
+            StringOperations stringOperations = new StringOperations();
+            bool actual = stringOperations.IsPalindrome("aabaa");
             Assert.True(actual);
-            }
+        }
+
+        [Fact]
+        public void ValidateStringNotReapeat()
+        {
+            string s = "abdghjk";
+            string actual = StringEx.IsValid(s);
+            Assert.Equal("YES", actual);
+        }
+
+        [Fact]
+        public void ValidateStringOneReapeat()
+        {
+            string s = "abdkghjk";
+            string actual = StringEx.IsValid(s);
+            Assert.Equal("YES", actual);
+        }
+        [Fact]
+        public void ValidateStringMultipleReapeat()
+        {
+            string s = "abdkkghjk";
+            string actual = StringEx.IsValid(s);
+            Assert.Equal("NO", actual);
+        }
+
+        [Fact]
+        public void ValidateStringMultipleReapeatLetters()
+        {
+            string s = "abdkkghjk";
+            string actual = StringEx.IsValid(s);
+            Assert.Equal("NO", actual);
+        }
+        [Fact]
+        public void ValidateStringSameReapeated()
+        {
+            string s = "abcdefghhgfedecba";
+            string actual = StringEx.IsValid(s);
+            Assert.Equal("YES", actual);
+        }
+        [Fact]
+        public void ValidateStringDiffReapeated()
+        {
+            string s = "aabbcd";
+            string actual = StringEx.IsValid(s);
+            Assert.Equal("NO", actual);
+        }
+[Fact]
+        public void ValidateStringTwoReapeated()
+        {
+            string s = "abbac";
+            string actual = StringEx.IsValid(s);
+            Assert.Equal("YES", actual);
+        }
     }
 }
