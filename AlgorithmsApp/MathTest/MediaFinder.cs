6c5822d29a3eb8d7b063e33bb5ba539d2ba14637
Elio Cortés <elio.cortes3000@gmail.com>
2023-06-21 12:46:16 -0500

    Implemented Median

---
+++ AlgorithmsApp/MathTest/MediaFinder.cs
@@ -0,0 +1,45 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AlgorithmsApp.MathTest
+{
+    public class MedianFinder
+    {
+        List<int> data;
+        public MedianFinder()
+        {
+            data = new List<int>();
+        }
+
+        public void AddNum(int num)
+        {
+            data.Add(num);
+        }
+
+        public double FindMedian()
+        {
+            double media = 0.0;
+            foreach (int val in data)
+            {
+                media += val;
+            }
+            
+            double result= media/data.Count;
+            int convert = (int)(result);
+            if (Math.Abs(result % convert)==0.5)
+            {
+                return result;
+            }
+            else
+            {
+                return Math.Round(result,0, MidpointRounding.AwayFromZero);
+            }
+        }
+    }
+
+}
+//public double FindMedian()
+//{
+//    double sum = data.Sum();
+//    double result = sum / data.Count;
+//    return Math.Round(result, MidpointRounding.AwayFromZero);
+//}
---
+++ TestProject/MedianFinderTests.cs
@@ -0,0 +1,28 @@
+﻿using AlgorithmsApp.MathTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class MedianFinderTests
+    {
+
+        [Fact]
+        public void ValidateMediaFinder()
+        {
+            MedianFinder medianFinder = new MedianFinder();
+            medianFinder.AddNum(2);    // arr = [1]
+            medianFinder.AddNum(3);    // arr = [1, 2]
+            medianFinder.AddNum(6);
+            medianFinder.AddNum(5);
+            medianFinder.AddNum(0);
+            double actual = medianFinder.FindMedian();
+            Assert.Equal(2.0, actual);
+
+        } 
+  
+    }
+}
